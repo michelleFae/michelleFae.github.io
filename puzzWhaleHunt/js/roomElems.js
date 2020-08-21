dd5ab22454d242455b3b542c69f91ac71be570f3
Michelle <Michelle.Dsouza@ww.com>
2020-08-21 02:15:48 +0100

    inital commit

---
+++ puzzWhaleHunt/js/roomElems.js
@@ -0,0 +1,64 @@
+var parameters, viewer, radius, button, position, infospot, timerId, easingItem, list, listItem, index = 0, baseScale = 300;
+
+
+  list = document.querySelector( '.mdl-list' );
+  listItem = document.querySelector( '.mdl-list__item' );
+
+  // Focus tweening parameter
+  parameters = {
+    amount: 50,
+    duration: 1000,
+    curve: 'Cubic',
+    easing: 'In',
+    iterative: false
+  };
+
+  // Init rooms
+  blackRoom = new PANOLENS.ImagePanorama('https://michelleFae.github.io/impactHack/assets/cubeWalls/blackRoom/finalRoom.png');
+  
+  // Recording image dimensions
+  const { edgeLength } = blackRoom;
+  radius = edgeLength / 2;
+
+
+  /* Insert Info Spots */
+
+  /* Main Room Items */
+  
+  //Share on social media
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(5000.00, -431.44, -4953.42), 'share-container', true,  blackRoom, "Share the Digital Love", "https://michelleFae.github.io/impactHack/assets/icons/shareIcon.png", radius / 3);
+
+
+  // whales
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(5000.00, -1961.48, 3770.59),"photo-container" , true, blackRoom, "Task #0: Photo Challenge", "https://michelleFae.github.io/impactHack/assets/officers/jacqueline_whale.png", radius/0.8);
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(-5000.00, -1725.68, 4881.38),"fullstop-container" , true, blackRoom, "Task #1: Find the Fullstop", "https://michelleFae.github.io/impactHack/assets/officers/prakash_whale.png", radius/0.8);
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(-5000.00, -1737.50, -4799.07),"officer-whale-container" , true, blackRoom, "Task #2: Where's that officer's whale?", "https://michelleFae.github.io/impactHack/assets/officers/valerie_whale.png", radius/0.8);
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(4193.59, -1720.01, -5000.00),"puzzle-container" , true, blackRoom, "Task #3: Puzzle Challenge", "https://michelleFae.github.io/impactHack/assets/officers/mantej_whale.png", radius/0.8);
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(2548.13, -1483.93, 5000.00),"scramble-container" , true, blackRoom, "Task #4: Unscramble!", "https://michelleFae.github.io/impactHack/assets/officers/chi_whale.png", radius/0.8);
+  linkPanoInfoSpotWtInfo(new THREE.Vector3(-2314.64, -1758.32, -5000.00),"done-container" , true, blackRoom, "DONE: Enter the Final Secret Word", "https://michelleFae.github.io/impactHack/assets/officers/tim_whale.png", radius/0.8);
+
+
+  // Add Scenes
+  addScenes();
+
+  // Removes the template div from list
+  list.removeChild(listItem);
+
+  // Set the navbar list to visible for the first room
+  setNavBarListInView(blackRoom);
+
+
+/* Set Intro vector - so the user sees ceiling on entrance into room */
+var lookAtPositions = [
+  new THREE.Vector3(188.35, 5000.00, 0.00)
+];
+
+blackRoom.addEventListener( 'enter-fade-start', function(){
+  viewer.tweenControlCenter( lookAtPositions[0], 0 );
+} );
+
+
+
+
+
+
---
+++ puzzWhaleHunt/js/roomFuncs.js
@@ -0,0 +1,95 @@
+  function linkPanoRooms (panorama1, panorama2, vect1, vect2, size, room1Name, room2Name, img1, img2) {
+    linkPanoInfoSpot(vect1, true, room2Name, panorama1, true, 1)
+    linkPanoInfoSpot(vect2, true, room1Name, panorama2, true, 1)
+    panorama1.linkWtMenuChange(panorama2, vect1, size, img1);
+    //link pano2 to main room
+    panorama2.linkWtMenuChange(panorama1, vect2, size, img2);
+
+
+
+  }
+
+  function addToNavBar(hoverText, infospot, panoramaRoom) {
+    var item = listItem.cloneNode( true );
+    item.classList.remove( 'template' );
+    item.querySelector( 'button' ).textContent = hoverText;
+    item.addEventListener( 'click', onFocus.bind( infospot ), false );
+    appendToNavBarList(panoramaRoom, item);
+  }
+
+
+  function linkPanoInfoSpot (vect, isAnimated, hoverText, panoramaRoom, isAddToNavBar, infoSpotSize=baseScale * vect.length() / radius) {
+    // sizing s.t. further away appears smaller
+
+    // Create info spot and add to sidebar
+    infospot = new PANOLENS.Infospot( infoSpotSize, PANOLENS.DataImage.Info, isAnimated);
+    infospot.position.copy( vect );
+    infospot.addHoverText(hoverText);
+    infospot.addEventListener('click', onFocus);
+    panoramaRoom.add(infospot);
+
+    // Add to left panel
+    if (isAddToNavBar) {
+      addToNavBar(hoverText, infospot, panoramaRoom);
+    }
+  }
+
+  function linkPanoInfoSpotWtInfo(vect, divContainer, isAddToNavBar, panoramaRoom, hoverText, image, infoSpotSize=undefined ) {
+    if (infoSpotSize == undefined) {
+      // sizing s.t. further away appears smaller
+      infoSpotSize = baseScale * vect.length() / radius;
+    }
+    
+    // add infospot with info that pops up in same room
+    infospot2 = new PANOLENS.Infospot( infoSpotSize, image);
+    infospot2.position.copy(vect);
+    infospot2.addHoverElement( document.getElementById(divContainer), 200 );
+
+    panoramaRoom.add(infospot2);
+
+    // Add to left panel
+    if (isAddToNavBar) {
+      addToNavBar(hoverText, infospot2, panoramaRoom);
+    }
+  }
+
+  function addInfoSpotFunc(item, _) {
+    linkPanoInfoSpotWtInfo (new THREE.Vector3(item.positionVector[0], item.positionVector[1], item.positionVector[2]), item.container, item.displayOnNavbar, item.panoramaRoom, item.title, item.image);
+  }
+
+
+  function onFocus () {
+    this.focus( parameters.duration, TWEEN.Easing[ parameters.curve ][ parameters.easing ] );
+  }
+
+
+/* Swaps divs with click from mainDivStr+initialNum to mainDivStr+NextNum*/
+  function SwapDivsWithClick(mainDivStr, initialNum, NextNum)
+  {
+     d1 = document.getElementById(mainDivStr + initialNum);
+     d2 = document.getElementById(mainDivStr + NextNum);
+     if( d2.style.display == "none" )
+     {
+        d1.style.display = "none";
+        d2.style.display = "block";
+     }
+     else
+     {
+        d1.style.display = "block";
+        d2.style.display = "none";
+     }
+  }
+
+
+
+
+  function addScenes() {
+    // Add panorama scenes to viewer
+    /* Add all scenes to viewer */ // todo: maybe change so can add 1 scene at a time
+
+    //viewer config: https://pchen66.github.io/Panolens/docs/index.html
+    viewer = new PANOLENS.Viewer( { output: 'console', container: document.querySelector( '#pcontainer' ), autoHideInfospot: false, cameraFov: 90} );
+    viewer.add(blackRoom, whiteRoom,redRoom, blueRoom); //todo: MANUALLY ADD NEW SCENES HERE
+    viewer.renderer.sortObjects = true;
+    
+  }
